fd2cc1e67bde319651b9265c64982c73bb3365cb
Aloys Augustin <aloys.augustin@polytechnique.org>
2017-09-20 12:12:47 +0200

    Good default for sourceAddressTranslation + better logging

--- webhook.py
+++ webhook.py
@@ -22,7 +22,7 @@ from f5.bigip import ManagementRoot
 
 config_file = './config_prod.json'
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
 
 # Configuration variables, taken from the environment
@@ -59,7 +59,7 @@ def webhook_listener():
         logging.info('Invalid request received')
         abort(404)
 
-    logging.info('Received %s event', data['eventName'])
+    logging.info('Received %s event for server %s', data['eventName'], data['data']['SCALR_SERVER_ID'])
 
     if data['eventName'] == 'HostUp':
         return add_host(data['data'])
@@ -70,10 +70,7 @@ def webhook_listener():
         return ''
 
 
-def add_host(data):
-    if not BIGIP_CONFIG_VARIABLE in data:
-        logging.info('This server should not be added in a BIG-IP pool, skipping.')
-        return 'Skipped'
+def parse_config_variable(data):
     lb_method = DEFAULT_LB_METHOD
     partition = DEFAULT_PARTITION
     config = [c.strip() for c in data[BIGIP_CONFIG_VARIABLE].split(',')]
@@ -90,6 +87,15 @@ def add_host(data):
         partition = config[5]
     if len(config) > 6:
         lb_method = config[6]
+    return pool_name, instance_port, vs_name, vs_address, vs_port, partition, lb_method
+
+
+def add_host(data):
+    if not BIGIP_CONFIG_VARIABLE in data:
+        logging.info('This server should not be added in a BIG-IP pool, skipping.')
+        return 'Skipped'
+
+    pool_name, instance_port, vs_name, vs_address, vs_port, partition, lb_method = parse_config_variable(data)
 
     vs_destination = '%s:%s' % (vs_address, vs_port)
 
@@ -102,6 +108,7 @@ def add_host(data):
             description='Scalr-managed pool',
             loadBalancingMode=lb_method)
     else:
+        logging.info('Pool %s already exists', pool_name)
         pool = client.tm.ltm.pools.pool.load(name=pool_name, partition=partition)
 
     if not client.tm.ltm.virtuals.virtual.exists(name=vs_name, partition=partition):
@@ -110,18 +117,21 @@ def add_host(data):
         virtual = client.tm.ltm.virtuals.virtual.create(
             name=vs_name,
             partition=partition,
-            description='Scalr-manages virtual server',
+            description='Scalr-managed virtual server',
             destination=vs_destination,
             mask='255.255.255.255',
             ipProtocol='tcp',
+            sourceAddressTranslation={'type': 'automap'},
             pool=pool_name)
+    else:
+        logging.info('Virtual server %s already exists, reusing it', vs_name)
 
     # Add server to pool
     server_ip = data['SCALR_INTERNAL_IP']
     server_name = '%s:%s' % (server_ip, instance_port)
     logging.info('Adding member %s in pool %s', server_name, pool_name)
     member = pool.members_s.members.create(partition=partition, name=server_name)
-
+    # TODO: in case of failure, check if it is because the server already exists
     return 'Ok'
 
 
@@ -129,22 +139,8 @@ def delete_host(data):
     if not BIGIP_CONFIG_VARIABLE in data:
         logging.info('This server should not be removed from a BIG-IP pool, skipping.')
         return 'Skipped'
-    lb_method = DEFAULT_LB_METHOD
-    partition = DEFAULT_PARTITION
-    config = [c.strip() for c in data[BIGIP_CONFIG_VARIABLE].split(',')]
-    if len(config) < 5:
-        # Invalid config
-        logging.warning('Invalid config received: %s', str(config))
-        abort(400, 'Invalid config passed: {}. Config format: {}'.format(config, config_format))
-    pool_name = config[0]
-    instance_port = config[1]
-    vs_name  = config[2]
-    vs_address = config[3]
-    vs_port = config[4]
-    if len(config) > 5:
-        partition = config[5]
-    if len(config) > 6:
-        lb_method = config[6]
+    
+    pool_name, instance_port, vs_name, vs_address, vs_port, partition, lb_method = parse_config_variable(data)
 
     if not client.tm.ltm.pools.pool.exists(name=pool_name, partition=partition):
         logging.info('Pool %s doesn\'t exist, has already been deleted', pool_name)
@@ -157,6 +153,8 @@ def delete_host(data):
         logging.info('Removing %s from pool %s', server_name, pool_name)
         member = pool.members_s.members.load(partition=partition, name=server_name)
         member.delete()
+    else:
+        logging.info('Member %s not found in pool, already deleted.', server_name)
 
     # Delete virtual server and pool if no members are left
     members = pool.members_s.get_collection()
@@ -166,7 +164,10 @@ def delete_host(data):
             logging.info('Deleting virtual server %s', vs_name)
             virtual_server = client.tm.ltm.virtuals.virtual.load(name=vs_name, partition=partition)
             virtual_server.delete()
+        logging.info('Deleting pool')
         pool.delete()
+    else:
+        logging.info('%d members remaining in pool, not deleting', len(members))
 
     return 'Ok'
 
